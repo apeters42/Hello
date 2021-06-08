9cea238cdeeb4b5bde073d8a8436aec1815d7a9b
apeters42 <85564810+apeters42@users.noreply.github.com>
2021-06-08 08:46:04 -0500

    Add files via upload

---
+++ hello_hack.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andrew Peterson and Jack Kohll
+ *         
+ * Date: 2021/06/07
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew Peterson and Jack Kohll\n");
+
+  return 0;
+}
